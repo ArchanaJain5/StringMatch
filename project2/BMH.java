a46f24f03b2e8855a3d99a5f3c8c331f4880dd0b
ArchanaJain5 <jainarchana998@gmail.com>
2023-04-29 13:52:27 -0700

    BMH added

--- project2/BMH.java
+++ project2/BMH.java
@@ -1,10 +1,37 @@
 package project2;
 
+import java.util.Arrays;
+
 public class BMH {
   
   public int match(String T, String P) {
     /** Your code goes here */
+    int[] shifts = computehueristic(P);
+    int lenP = P.length() ,lenT = T.length();
+    int i = lenP-1,j=lenP-1;
+
+    while(i<lenT){
+      if(T.charAt(i)==P.charAt(j)){
+        if(j==0) return i;
+        i--;
+        j--;
+      }
+      else{
+        int jumps = shifts[T.charAt(i)-'c'];
+        i = i+lenP - Math.min(j,1+jumps);
+        j=lenP-1;
+      }
+    }
     return -1;
   }
-
+  private int[] computehueristic(String P){
+    int[] shifts = new int[256];
+    int lenP = P.length();
+    Arrays.fill(shifts,-1);
+    for(int i=0;i<lenP;i++){
+      char c = P.charAt(i);
+      shifts[c-'a']=i;
+    }
+    return shifts;
+  }
 }
